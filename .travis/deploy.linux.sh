9fb38d79471d666863c3aff7e2448baf7f8518c8
Frederick Thomssen <thomssen@thomssen-it.de>
2016-10-27 12:19:02 +0200

    Build .deb and .rpm

--- .travis/deploy.linux.sh
+++ .travis/deploy.linux.sh
@@ -1,34 +1,51 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <output file>"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <output prefix> <version>"
   exit 1
 fi
 
 export TRAVISDIR=$(cd "$(dirname "$0")"; pwd)
 export ROOTDIR=$TRAVISDIR/..
-export DEPLOYFILE=$1
+export PREFIX=$1
+export VERSION=$2
 
 cd $ROOTDIR
 
+##################### AppImage creation #####################
+
 # Create a new dist folder
 rm -rf dist
-mkdir dist
+mkdir -p dist/opt/redtimer
 
 # Include binary and dist files
-cp client/RedTimerClient dist
-cp client/icons/clock_red.svg dist
-cp .travis/RedTimerClient.desktop dist
+cp client/RedTimerClient dist/opt/redtimer
+cp client/icons/clock_red.svg dist/opt/redtimer/redtimer.svg
+cp .travis/RedTimerClient.desktop dist/opt/redtimer
 
 # Include SVG icon engine
-mkdir -p dist/plugins/iconengines
-cp -a /opt/qt57/plugins/iconengines/libqsvgicon.so dist/plugins/iconengines
+# @todo Integrate into deploylinuxqt
+mkdir -p dist/opt/redtimer/plugins/iconengines
+cp -a /opt/qt57/plugins/iconengines/libqsvgicon.so dist/opt/redtimer/plugins/iconengines
 
 # First run
-.travis/linuxdeployqt.AppImage dist/RedTimerClient -qmldir=client/qml -appimage -bundle-non-qt-libs -verbose=2
+.travis/linuxdeployqt.AppImage dist/opt/redtimer/RedTimerClient -qmldir=client/qml -verbose=2
 
 # Second run, to include xcbglintegration
-rm dist.AppImage
-.travis/linuxdeployqt.AppImage dist/RedTimerClient -qmldir=client/qml -appimage -bundle-non-qt-libs -verbose=2
+.travis/linuxdeployqt.AppImage dist/opt/redtimer/RedTimerClient -qmldir=client/qml -appimage -bundle-non-qt-libs -verbose=2
 
-mv dist.AppImage $DEPLOYFILE
+mv dist/opt/redtimer.AppImage $PREFIX.AppImage
+
+##################### DEB/RPM creation #####################
+
+mkdir -p dist/usr/bin
+mkdir -p dist/usr/share/applications
+mkdir -p dist/usr/share/icons/hicolor/scalable/apps
+
+ln -sf /opt/redtimer/RedTimerClient dist/usr/bin/RedTimerClient
+mv dist/opt/redtimer/RedTimerClient.desktop dist/usr/share/applications
+mv dist/opt/redtimer/redtimer.svg dist/usr/share/icons/hicolor/scalable/apps
+rm -f dist/opt/redtimer/default.png
+
+fpm -s dir -t deb -n redtimer -v $VERSION -C dist -p $PREFIX.deb
+fpm -s dir -t rpm -n redtimer -v $VERSION -C dist -p $PREFIX.rpm
