85843cb2693cd5d87007d5a98db1573bdbaacbb3
Frederick Thomssen <thomssen@thomssen-it.de>
2016-11-24 12:44:53 +0100

    Fix deb signature

--- .travis/deploy.linux.sh
+++ .travis/deploy.linux.sh
@@ -59,20 +59,24 @@ mv dist/opt/redtimer/redtimer.desktop dist/usr/share/applications
 mv dist/opt/redtimer/redtimer.svg dist/usr/share/icons/hicolor/scalable/apps
 rm -f dist/opt/redtimer/default.png
 
+# Add Debian-specific files
+export TIMESTAMP=$(date -R)
+cat > CHANGELOG <<- END
+redtimer ($VERSION) unstable; urgency=low
+
+  * Dummy changelog to please lintian
+
+ -- Frederick Thomssen <thomssen@thomssen-it.de>  $TIMESTAMP
+END
+
+mkdir -p dist/usr/share/doc/redtimer
+cp $TRAVISDIR/debian/copyright dist/usr/share/doc/redtimer
+
 # Set correct filesystem permissions
 find dist/* -type d -exec chmod 755 {} \;
 find dist/* -type f -exec chmod 644 {} \;
 chmod 755 dist/opt/redtimer/redtimer
 chmod 755 dist/opt/redtimer/redtimercli
-
-set -x
-
-# Add Debian-specific files
-mkdir -p dist/usr/share/doc/redtimer
-echo "RedTimer version $VERSION" > dist/usr/share/doc/redtimer/changelog
-gzip dist/usr/share/doc/redtimer/changelog
-export YEAR=$(date +%Y)
-echo "Copyright (c) ${YEAR} by Frederick Thomssen <thomssen@thomssen-it.de>" > dist/usr/share/doc/redtimer/copyright
 
 export MAINT="Frederick Thomssen <thomssen@thomssen-it.de>"
 export DESCR="Redmine Time Tracker\n\n
@@ -80,7 +84,10 @@ RedTimer is an easy-to-use platform-independent time tracker which allows the\n
 user to track time while working on an issue. Furthermore, a user can create\n
 new issues using RedTimer during which the time tracking will already start."
 
-export OPTS="-m \"$MAINT\" --description \"$DESCR\" -n redtimer -v $VERSION -C dist --no-depends --category net"
+rm -f $PREFIX.deb $PREFIX.rpm
 
-fpm -s dir -t deb -p $PREFIX.deb $OPTS --deb-changelog CHANGELOG
-fpm -s dir -t rpm -p $PREFIX.rpm $OPTS
+# .deb
+fpm -s dir -t deb -p $PREFIX.deb -m "${MAINT}" --description "${DESCR}" -n redtimer -v ${VERSION} -C dist --category net --deb-no-default-config-files --deb-changelog CHANGELOG --no-depends
+
+# .rpm
+fpm -s dir -t rpm -p $PREFIX.rpm -m "${MAINT}" --description "${DESCR}" -n redtimer -v ${VERSION} -C dist --category net --deb-no-default-config-files
